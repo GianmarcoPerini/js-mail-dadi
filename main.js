b3f082f43d251305d6d8aca92e15d1c51e2afced
GianmarcoPerini <g.marco.perini@gmail.com>
2021-08-06 14:57:19 +0200

    init

---
+++ main.js
@@ -0,0 +1,14 @@
+const accessoConsentito = ["Marco", "Giovanni", "Laura", "Francesca", "Joele", "Michael", "John", "Gianmarco"];
+
+
+let verificaAccesso = prompt("Inserisci il tuo nome");
+
+    
+    if( verificaAccesso === accessoConsentito){
+    console.log("accedi")
+    } else {
+    console.log("fuori")
+    }
+
+
+
